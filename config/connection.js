61bc9de1976e3edb5abea7932afd07dcdaef4cc6
Munshid <muhammadmunshid200@gmail.com>
2023-07-29 16:15:44 +0530

    mongoose updated

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
 module.exports.connect = function () {
-    mongoose.connect('mongodb://mongodb+srv://munshid:munshid123@cluster0.fyiocsw.mongodb.net/job-portal')
+    mongoose.connect('mongodb+srv://munshid:munshid123@cluster0.fyiocsw.mongodb.net/event-tech?retryWrites=true&w=majority/job-portal')
     console.log('mongoose connected');
 }
